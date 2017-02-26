9f4dd4e8f1090e27241c4d49aa1c5bdc0a5c73dd
Mike O'Malley <spuriousdata@gmail.com>
2017-02-26 11:12:26 -0500

    kill unused files

--- r53dyndns/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from r53dyndns.views import ListView, UpdateView
-
-urlpatterns = patterns('',
-        url(r'^list/$', ListView.as_view(), name='list'),
-        url(r'^update/(?P<fqdn>[\w\.]+)/$', UpdateView.as_view(), name='update'),
-)
